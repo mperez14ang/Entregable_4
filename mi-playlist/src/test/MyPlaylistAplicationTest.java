c35788293459c2b6b58ffd28bc71abdd1a7fea08
Manuel Pérez Angenscheidt <146390467+mperez14ang@users.noreply.github.com>
2025-11-11 09:57:48 -0300

    .

---
+++ mi-playlist/src/test/MyPlaylistAplicationTest.java
@@ -0,0 +1,12 @@
+package com.um.miplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPlaylistApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
